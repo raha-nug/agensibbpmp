2ff17c4c0dc096627ec1be610f71c713c8645a2d
raha-nug <rival10221029@student.sttcipasung.ac.id>
2025-02-01 19:26:46 +0700

    add vercel config

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const authRoute = require("./routes/auth.routes");
 const agensiRoute = require("./routes/agensi.routes");
